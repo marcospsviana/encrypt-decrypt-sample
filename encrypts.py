bf71338aff0c26982495e097017220a5fa9b73a6
marcospsviana <marcospaulo.silvaviana@gmail.com>
2024-12-15 17:02:27 -0300

    update encrypts put public key inside function encrypt

--- encrypts.py
+++ encrypts.py
@@ -2,17 +2,16 @@ import gnupg
 
 gpg = gnupg.GPG(gnupghome="/tmp")
 
-with open("public.pem", "rb") as file:
-    results = gpg.import_keys(file.read())
-    if results.counts == 0:
-        raise ImportError("Fail to import public key")
-public_key = gpg.list_keys()
-fingerprint = public_key.fingerprints
-
-    
 
 def encrypt_file(file_path, output_file):
     try:
+        with open("public.pem", "rb") as file:
+            results = gpg.import_keys(file.read())
+            if results.counts == 0:
+                raise ImportError("Fail to import public key")
+        public_key = gpg.list_keys()
+        fingerprint = public_key.fingerprints
+    
         with open(file_path, "rb") as f:
             status = gpg.encrypt_file(f, recipients=fingerprint, output=output_file)
             if status.ok:
@@ -22,5 +21,3 @@ def encrypt_file(file_path, output_file):
                 raise
     except Exception as err:
         raise f"Exception error to try encrypt file: {err}"
-        
-
