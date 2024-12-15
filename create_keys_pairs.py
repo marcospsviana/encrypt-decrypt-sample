04f4c313d390d8350c3d5949c19ff9038c34bd5e
marcospsviana <marcospaulo.silvaviana@gmail.com>
2024-12-15 11:13:51 -0300

    update create keys source

--- create_keys_pairs.py
+++ create_keys_pairs.py
@@ -4,35 +4,17 @@ import gnupg
 gpg = gnupg.GPG(gnupghome="/tmp")
 gpg.encoding = "utf-8"
 
-encrypt_type = None
-pass_phrase = input("Input a passphrase")
-pass_phrase = input("Input an email")
-options_map = {
-                "1": "RSA",
-                "2": "ECC",
-                "3": "DH",
-                "4": "ECDH",
-                "5": "DSA",
-                "6": "EdDSA",
-            }
-print("===== choose type encryptation =======\n")
-options = """
-1 - RSA 
-2 - ECC
-3 - DH
-4 - ECDH
-5 - DSA
-6 - EdDSA
-7 - exit
-\n"""
-print(options)
-exit = False
-while not exit:
-    choose_type = input("Enter option type encrypt")
-    if choose_type == "7":
-        break
-    try:
-        encrypt_type = options_map[choose_type]
-    except ValueError:
-        print("Incorrect option")
 
+def make_keys():
+    input_data = gpg.gen_key_input(
+                    name_real='name',
+                    name_email='name@email.com',
+                    passphrase='passphrase',
+                    key_type='RSA',
+                    key_length =2048,
+                    expire_date='2y'
+                )
+    gpg.gen_key(input_data)
+
+
+make_keys()
