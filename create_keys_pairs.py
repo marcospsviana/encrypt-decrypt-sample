bdd0ccc2ff0a302773ad26e3eab0f436e7720e0a
marcospsviana <marcospaulo.silvaviana@gmail.com>
2024-12-15 16:52:00 -0300

    update encrypts and create keys

--- create_keys_pairs.py
+++ create_keys_pairs.py
@@ -5,7 +5,7 @@ gpg = gnupg.GPG(gnupghome="/tmp")
 gpg.encoding = "utf-8"
 
 
-def make_keys():
+def generate_keys_pair():
     input_data = gpg.gen_key_input(
                     name_real='name',
                     name_email='name@email.com',
@@ -15,6 +15,11 @@ def make_keys():
                     expire_date='2y'
                 )
     gpg.gen_key(input_data)
+    key_ids = [g["keyid"] for g in gpg.list_keys(True)]
+    gpg.export_keys(keyids=key_ids, secret=True, passphrase="passphrase", output="private.pem")
+    gpg.export_keys(keyids=key_ids,  passphrase="passphrase", output="public.pem")
+    
+    
 
 
-make_keys()
+generate_keys_pair()
--- encrypts.py
+++ encrypts.py
@@ -1,4 +1,26 @@
 import gnupg
 
-gpg = gnupg.GPG(gnupghome="$HOME/.gnupg")
+gpg = gnupg.GPG(gnupghome="/tmp")
+
+with open("public.pem", "rb") as file:
+    results = gpg.import_keys(file.read())
+    if results.counts == 0:
+        raise ImportError("Fail to import public key")
+public_key = gpg.list_keys()
+fingerprint = public_key.fingerprints
+
+    
+
+def encrypt_file(file_path, output_file):
+    try:
+        with open(file_path, "rb") as f:
+            status = gpg.encrypt_file(f, recipients=fingerprint, output=output_file)
+            if status.ok:
+                print(f"File {file_path} encrypted successfull")
+            else:
+                print(f"Fail to ecrypt file: {status.stderr}")
+                raise
+    except Exception as err:
+        raise f"Exception error to try encrypt file: {err}"
+        
 
